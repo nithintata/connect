ad8ace8f9bee8cd40aaf7df021d0ed4374dc97f6
nithintata <nithin.tata2000@gmail.com>
2020-05-24 21:33:24 +0530

    implementing like and unlike on client side

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,7 +1,9 @@
-import React, {useState, useEffect} from 'react'
+import React,{useEffect, useState, useContext} from 'react'
+import {UserContext} from '../../App'
 
 const Home = () => {
   const [data, setData] = useState([])
+  const {state, dispatch} = useContext(UserContext)
   useEffect(() => {
     fetch('/posts/all', {
       headers: {
@@ -9,9 +11,57 @@ const Home = () => {
       }
     }).then(res => res.json())
     .then(result => {
+      console.log(result)
       setData(result)
     })
   }, [])
+
+  const likePost = (id) => {
+    fetch('/posts/like', {
+      method: "put",
+      headers: {
+        "Content-Type" : "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("jwt")
+      },
+      body: JSON.stringify({
+        postId: id
+      })
+    }).then(res => res.json()).then(result => {
+      const newData = data.map(item => {
+        if (item._id == result._id) {
+          return result
+        }
+        else {
+          return item
+        }
+      })
+      setData(newData)
+    }).catch(err => console.log(err))
+  }
+
+  const unlikePost = (id) => {
+    fetch('/posts/unlike', {
+      method: "put",
+      headers: {
+        "Content-Type" : "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("jwt")
+      },
+      body: JSON.stringify({
+        postId: id
+      })
+    }).then(res => res.json()).then(result => {
+      const newData = data.map(item => {
+        if (item._id == result._id) {
+          return result
+        }
+        else {
+          return item
+        }
+      })
+      setData(newData)
+    }).catch(err => console.log(err))
+  }
+
   return (
     <div className = "home">{
       data.map(item => {
@@ -23,6 +73,11 @@ const Home = () => {
             </div>
             <div className = "card-content">
             <i className = "material-icons" style = {{color:"red"}}>favorite</i>
+            {item.likes.includes(state._id)
+            ? <i className = "material-icons" onClick = {() => {unlikePost(item._id)}}>thumb_down</i>
+            : <i className = "material-icons" onClick = {() => {likePost(item._id)}}>thumb_up</i>
+            }
+              <h6>{item.likes.length} likes</h6>
               <h5>{item.title}</h5>
               <p>{item.body}</p>
               <input type = "text" placeholder = "comment on this post" />
