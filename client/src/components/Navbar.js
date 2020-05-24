e30f88e929bf4916bcf404bbf888af18f9ab34b4
nithintata <nithin.tata2000@gmail.com>
2020-05-24 13:40:08 +0530

    changing nav links dynamically

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {Link} from 'react-router-dom'
+import {UserContext} from '../App'
+
 const NavBar = () => {
+  const {state, dispatch} = useContext(UserContext)
+  const renderList = () => {
+    if (state) {
+      return [
+        <li><Link to="/profile">Profile</Link ></li>,
+        <li><Link to="/create-post">Create Post</Link ></li>
+      ]
+    }
+    else {
+      return [
+        <li><Link to="/signin">Signin</Link ></li>,
+        <li><Link to="/signup">Signup</Link ></li>
+      ]
+    }
+  }
+
+
   return (
     <nav>
     <div className="nav-wrapper white">
-      <Link to="/" className="brand-logo myfont left">Connect</Link >
+      <Link to= {state ? "/" : "/signin"} className="brand-logo myfont left">Connect</Link >
       <ul id="nav-mobile" className="right">
-        <li><Link to="/signin">Signin</Link ></li>
-        <li><Link to="/signup">Signup</Link ></li>
-        <li><Link to="/profile">Profile</Link ></li>
-        <li><Link to="/create-post">Create Post</Link ></li>
+        {renderList()}
       </ul>
     </div>
   </nav>
