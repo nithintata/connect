cb5928b7d43cd6c9b4599fb7302e9c7e077f2b84
nithintata <nithin.tata2000@gmail.com>
2020-05-24 13:29:25 +0530

    Added context API

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,61 @@
-import React from 'react';
+import React,{useEffect, createContext, useReducer, useContext} from 'react';
 import NavBar from './components/Navbar'
 import "./App.css"
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Switch, useHistory} from 'react-router-dom'
 import Home from './components/screens/Home'
 import Profile from './components/screens/Profile'
 import Signin from './components/screens/Signin'
 import Signup from './components/screens/Signup'
 import CreatePost from './components/screens/CreatePost'
+import {reducer, initialState} from './reducers/userReducer'
+
+export const UserContext = createContext()
+
+const Routing = () => {
+  const history = useHistory()
+  const {state, dispatch} = useContext(UserContext)
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"))
+    if (user) {
+      dispatch({type: "USER", payload: user})
+      history.push('/')
+    }
+    else {
+      history.push('/signin')
+    }
+  }, [])
+  return (
+    <Switch>
+    <Route exact path = "/">
+      <Home />
+    </Route>
+    <Route path = "/signin">
+      <Signin />
+    </Route>
+    <Route path = "/signup">
+      <Signup />
+    </Route>
+    <Route path = "/profile">
+      <Profile />
+    </Route>
+    <Route path = "/create-post">
+      <CreatePost />
+    </Route>
+    </Switch>
+  )
+}
+
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
+    <UserContext.Provider value = {{state, dispatch}}>
     <BrowserRouter>
       <NavBar />
-      <Route exact path = "/">
-        <Home />
-      </Route>
-      <Route path = "/signin">
-        <Signin />
-      </Route>
-      <Route path = "/signup">
-        <Signup />
-      </Route>
-      <Route path = "/profile">
-        <Profile />
-      </Route>
-      <Route path = "/create-post">
-        <CreatePost />
-      </Route>
+      <Routing />
+
     </BrowserRouter>
+    </UserContext.Provider>
   )
 }
 
--- client/src/components/screens/Signin.js
+++ client/src/components/screens/Signin.js
@@ -1,8 +1,10 @@
-import React, {useState} from 'react'
+import React, {useState, useContext,} from 'react'
 import {Link, useHistory} from 'react-router-dom'
 import M from 'materialize-css'
+import {UserContext} from '../../App'
 
 const Signin = () => {
+  const {state, dispatch} = useContext(UserContext)
   const history = useHistory()
   const [password, setPassword] = useState("")
   const [email, setEmail] = useState("")
@@ -23,6 +25,7 @@ const Signin = () => {
       else {
         localStorage.setItem("jwt", data.token)
         localStorage.setItem("user", JSON.stringify(data.user))
+        dispatch({type: "USER", payload: data.user})
         M.toast({html: "Login Success", classes: "#43a047 green darken-1"})
         history.push('/')
       }
