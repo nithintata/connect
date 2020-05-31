a6574c1284ac1225eb06c9235d6860ce9897f2d0
nithintata <nithin.tata2000@gmail.com>
2020-05-31 13:16:12 +0530

    err fix

--- app.js
+++ app.js
@@ -34,11 +34,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-})
 
 
 app.use('/', indexRouter);
