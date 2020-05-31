2ca6718c6340a0bc3920d0c24d708e048c8b1b00
nithintata <nithin.tata2000@gmail.com>
2020-05-31 20:15:05 +0530

    env variables

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'secretKey': '17545-67830-09476-56321',
-    'mongoUrl': 'mongodb+srv://nithin:2MYYREGdvDiT1O4O@cluster0-upfz1.mongodb.net/test?retryWrites=true&w=majority'
+    'secretKey': process.env.JWT_SEC,
+    'mongoUrl': process.env.MONGO_URI
 }
